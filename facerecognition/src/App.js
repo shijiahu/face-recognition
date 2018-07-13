c1032f682cc2e38252fdbc5e14d7458242228b5b
Shijia Hu <shijiahu2016a@gmail.com>
2018-07-13 19:16:31 -0400

    fix bug on responsive problem

--- facerecognition/src/App.js
+++ facerecognition/src/App.js
@@ -37,7 +37,8 @@ class App extends Component {
   }
 
   calculateFaceLocation = (data) => {
-    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    console.log(data);
+        const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputimage');
     const width = Number(image.width);
     const height = Number(image.height);
