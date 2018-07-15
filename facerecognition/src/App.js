9f7d55bca7f167e7a2139ba6a1c6cd889ef2177b
Shijia Hu <shijiahu2016a@gmail.com>
2018-07-14 22:55:30 -0400

    connect signin function with backend

--- facerecognition/src/App.js
+++ facerecognition/src/App.js
@@ -40,6 +40,12 @@ class App extends Component {
     }
   }
 
+  componentDidMount(){
+    fetch('http://localhost:3000')
+      .then(response => response.json())
+      .then(console.log);
+  }
+
   calculateFaceLocation = (data) => {
     console.log(data);
         const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
