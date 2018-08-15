8173980e82ac0716fd1086dfb1a1516110477b4d
Shijia Hu <shijiahu2016a@gmail.com>
2018-08-15 02:37:47 -0700

    test for mutiply faces

--- src/App.js
+++ src/App.js
@@ -58,17 +58,31 @@ class App extends Component {
   // }
 
   calculateFaceLocation = (data) => {
-    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
-    const image = document.getElementById('inputimage');
-    const width = Number(image.width);
-    const height = Number(image.height);
-    return {
-      leftCol: clarifaiFace.left_col * width,
-      topRow: clarifaiFace.top_row * height,
-      rightCol: width - (clarifaiFace.right_col * width),
-      bottomRow: height - (clarifaiFace.bottom_row * height)
-    }
+    // const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    // const image = document.getElementById('inputimage');
+    // const width = Number(image.width);
+    // const height = Number(image.height);
+    // return {
+    //   leftCol: clarifaiFace.left_col * width,
+    //   topRow: clarifaiFace.top_row * height,
+    //   rightCol: width - (clarifaiFace.right_col * width),
+    //   bottomRow: height - (clarifaiFace.bottom_row * height)
+    // }
+    const image = document.getElementById('inputImage')
+    const width = Number(image.width)
+    const height = Number(image.height) 
+    const newArr = data.outputs[0].data.regions.map(region => {
+      return{
+        leftCol:region.region_info.bounding_box.left_col * width,
+        topRow: region.region_info.bounding_box.top_row * height,
+        rightCol:width - (region.region_info.bounding_box.right_col * width),
+        bottomRow: height - (region.region_info.bounding_box.bottom_row * height)
+      } 
+    })
+    return newArr
   }
+
+
 
   displayFaceBox = (box)=>{
     console.log(box);
@@ -129,7 +143,9 @@ class App extends Component {
               <ImageLinkForm 
                 OnInputChange = {this.OnInputChange} 
                 OnButtonSubmit = {this.OnButtonSubmit}/>
-              <FaceRecognition box = {this.state.box} imageUrl = {this.state.imageUrl}/>
+              <FaceRecognition imageUrl={imgUrl} calcFaceLocation={this.calcFaceLocation} box={box} />
+              {/*<FaceRecognition box = {this.state.box} imageUrl = {this.state.imageUrl}/>*/}
+              
             </div>
           : ( this.state.route === 'signin'
               ? <SignIn loadUser= {this.loadUser} onRouteChange = {this.onRouteChange}/>
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -2,14 +2,30 @@ import React from 'react';
 import './FaceRecognition.css';
 
 const FaceRecognition = ({imageUrl, box}) => {
-	return (
-		<div className='center ma'>
-			<div className = 'absolute mt2'>
-				<img id="inputimage" alt='' src={imageUrl} width='500px' height='auto'/>
-				<div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
-			</div>
-		</div>
-	);
+	const facesArr = ()=> {
+	    if(box){
+	     const newArr =  box.map(face => {
+	        return <div className="bounding-box" key={shortid.generate()} style={{top:face.topRow,left:face.leftCol,right:face.rightCol, bottom:face.bottomRow}}></div>
+	      })
+	      return newArr
+	    }
+	    return null
+	 }
+
+	  return (
+	    <div style={{position:'relative',width:'30%',margin:'0 auto'}}>
+	      {imageUrl?<img id="inputImage"  src={imageUrl} alt="recognImage" style={{position:'relative'}} className="imageFaces"/>:null}
+	      {facesArr()}
+	    </div>
+	  )
+	// return (
+	// 	<div className='center ma'>
+	// 		<div className = 'absolute mt2'>
+	// 			<img id="inputimage" alt='' src={imageUrl} width='500px' height='auto'/>
+	// 			<div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
+	// 		</div>
+	// 	</div>
+	// );
 };
 
 export default FaceRecognition;
